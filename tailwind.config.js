df49d39ca92f54af9fef4d82d4fc72cd4b3ad9fe
Michael Veselov <michael.veselov@gmail.com>
2023-04-23 18:58:53 +0300

    changed project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./public/**/*.html'],
   theme: {
     extend: {
       gridTemplateRows: {
@@ -11,7 +11,7 @@ module.exports = {
       },
       fontFamily: {
         main: ['Montserrat'],
-        ubuntu: ['Ubuntu']
+        ubuntu: ['Ubuntu'],
       },
     },
   },
